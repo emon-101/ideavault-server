d1b87af4315069316604aa89883141994da7d37a
emon-101 <emon26.ugv@gmail.com>
2026-06-07 22:44:47 +0600

    feat: add JWT authentication middleware using jose-cjs for token verification

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv = require("dotenv");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { createRemoteJWKSet, jwtVerify } = require("jose-cjs");
 dotenv.config();
 const uri = process.env.MONGODB_URI;
 
@@ -18,6 +19,30 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+
+const JWKS = createRemoteJWKSet(
+  new URL("http://localhost:3000/api/auth/jwks")
+)
+
+const verifyToken = async(req, res, next) => {
+  const authHeader = req?.headers.authorization
+  if(!authHeader) {
+    return res.status(401).json({message: "Unauthorized"});
+  }
+  const token = authHeader.split(" ")[1]
+  if(!token) {
+    return res.status(401).json({message: "Unauthorized"});
+  }
+  console.log(token);
+
+  try{
+    const{payload} = await jwtVerify(token, JWKS)
+    console.log(payload);
+    next();
+  } catch(error) {
+    return res.status(403).json({message: "Forbidden"})
+  }
+}
 
 async function run() {
   try {
@@ -226,7 +251,7 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/idea/:id", async (req, res) => {
+    app.get("/idea/:id",verifyToken, async (req, res) => {
       const { id } = req.params;
       const result = await ideaCollection.findOne({ _id: new ObjectId(id) });
       res.send(result);
